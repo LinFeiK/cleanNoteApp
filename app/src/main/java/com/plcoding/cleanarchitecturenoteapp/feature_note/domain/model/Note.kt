6a5ae897331edde9c4f52c86ed00ad134775f383
LFK <linfeikang123@gmail.com>
2025-09-18 04:13:42 -0400

    chore: Set up Note data, database, dao, and repository.

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,22 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.BabyBlue
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.LightGreen
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.RedOrange
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.RedPink
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.Violet
+
+@Entity
+data class Note(
+    @PrimaryKey val id: Int? = null,
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val colour: Int
+) {
+    companion object {
+        val noteColours = listOf(BabyBlue, LightGreen, RedOrange, RedPink, Violet)
+    }
+}
